1188aaa9716a13ab4a9a702c35a5cdcaf15c139e
catalinnega <negacevschi.catalin@gmail.com>
2024-07-19 15:11:04 +0000

    first test

---
+++ tests/test_init_camera.py
@@ -0,0 +1,14 @@
+import pytest
+from lit_checker.args import GlobalConfig
+from lit_checker.camera.camera_processor import CameraProcessor
+
+
+def test_init_camera(yaml_config_path: str) -> None:
+    config = GlobalConfig.from_yaml(yaml_config_path)
+    camera_processor = CameraProcessor(config, verbose=False)
+    assert camera_processor is not None, "Failed to build CameraProcessor"
+
+
+@pytest.fixture
+def yaml_config_path() -> str:
+    return 'configs/c100/config_c100.yaml'
